6ae491f6ee5d2478b7e6ff58ae7386399b55df51
Ahmed-rbr <ahmed.rabar.dev@gmail.com>
2025-07-09 23:06:21 +0100

    dd

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,5 +1,9 @@
 number1 =10,number2 =5
+addition=number1+number2
+subtraction=number1-number2
 
-print(f"Addition of 10 and 5 is {number1+number2}")
-print(f"Subtraction of 10 and 5 is  {number1-number2}")
-print(f"Multiplication of 10 and 5 is {number1*number2}")
+multiplication=number1*number2
+
+print(f"Addition of 10 and 5 is {addition}")
+print(f"Subtraction of 10 and 5 is  {subtraction}")
+print(f"Multiplication of 10 and 5 is {multiplication}")
