f23d75f6cf745909497b2b89c9337e8aaff0e003
Ahmed-rbr <ahmed.rabar.dev@gmail.com>
2025-07-09 22:27:11 +0100

    first task finished

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,6 @@
+number1=5
+number2=10
+
+print(f"Addition of 10 and 5 is {number1+number2}")
+print(f"Subtraction of 10 and 5 is  {number2-number1}")
+print(f"Multiplication of 10 and 5 is {number1*number2}")
