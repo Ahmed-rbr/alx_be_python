939eb8cdd3a3c0b8ba5f8e89c07a48edb311c5dc
Ahmed-rbr <ahmed.rabar.dev@gmail.com>
2025-07-24 16:05:25 +0100

    datetime in py

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,17 @@
+from datetime import datetime, timedelta
+
+
+def display_current_datetime():
+
+    current_date = datetime.now()
+    print(f"Current date and time:", current_date)
+
+
+def calculate_future_date():
+    current_date = datetime.now()
+    days = int(input('Enter the number of days to add to the current date'))
+    future_date = current_date+timedelta(days=days)
+    print(f"Future date:", future_date.strftime("%Y-%m-%d"))
+
+
+calculate_future_date()
