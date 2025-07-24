4ab9c0e081132bd0996a37f23ad00eb7da437f75
Ahmed-rbr <ahmed.rabar.dev@gmail.com>
2025-07-24 04:11:56 +0100

    fixed err

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,15 +1,23 @@
-from arithmetic_operations import perform_operation
-
-
 def perform_operation():
     print("Arithmetic Operations")
     num1 = float(input("Enter the first number: "))
     num2 = float(input("Enter the second number: "))
     operation = input(
         "Enter the operation (add, subtract, multiply, divide): ").strip().lower()
-    result = perform_operation(num1, num2, operation)
-    print(f"Result: {result}")
+    if operation not in ('+', '-', '*', '/'):
+        print('something went Wrong!')
+    match operation:
+        case '+':
+            print(f"The result is {num1 + num2}")
+        case '-':
+            print(f"The result is {num1 - num2}")
+        case '*':
+            print(f"The result is {num1 * num2}")
+        case '/':
+            if num2 == 0:
+                print("Cannot divide by zero.")
+            else:
+                print(f"The result is {num1 / num2}")
 
 
-if __name__ == "__main__":
-    main()
+perform_operation()
