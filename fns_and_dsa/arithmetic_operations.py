c44f7630c7ca77655175479caae76a4349238529
Ahmed-rbr <ahmed.rabar.dev@gmail.com>
2025-07-24 04:15:57 +0100

    done

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,14 +1,14 @@
 def perform_operation(num1, num2, operation):
-    if operation not in ('+', '-', '*', '/'):
+    if operation not in (add, subtract, multiply, divide):
         print('something went Wrong!')
     match operation:
-        case '+':
+        case 'add':
             print(f"The result is {num1 + num2}")
-        case '-':
+        case 'subtract':
             print(f"The result is {num1 - num2}")
-        case '*':
+        case 'multiply':
             print(f"The result is {num1 * num2}")
-        case '/':
+        case 'divide':
             if num2 == 0:
                 print("Cannot divide by zero.")
             else:
