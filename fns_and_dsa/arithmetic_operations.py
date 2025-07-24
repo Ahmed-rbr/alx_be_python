3f5f3f439c2525019d269972d56b3f507b6384ab
Ahmed-rbr <ahmed.rabar.dev@gmail.com>
2025-07-24 04:19:27 +0100

    don33333

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -9,11 +9,11 @@ def perform_operation(num1, num2, operation):
         case 'multiply':
             print(f"The result is {num1 * num2}")
         case 'divide':
-            if num2 == 0:
+            if num2 != 0:
+                print(f"The result is {num1 / num2}")
+            elif num2 == 0:
                 print("Cannot divide by zero.")
                 return
-            else:
-                print(f"The result is {num1 / num2}")
 
 
 perform_operation()
